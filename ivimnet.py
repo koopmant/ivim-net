35aa7bd9a899027d049473d13660664563950dfa
Thomas Koopman <thomas.koopman@quest-innovations.com>
2020-04-08 18:49:21 +0200

    changed to sigmoid

--- ivimnet.py
+++ ivimnet.py
@@ -15,17 +15,23 @@ class IVIMNet(nn.Module):
                                      nn.Linear(len(b_values), 4))
 
     def forward(self, x):
-        params = torch.abs(self.encoder(x))
+        params = torch.sigmoid(self.encoder(x))
 
-        dp = params[:, 0].unsqueeze(1)
-        dt = params[:, 1].unsqueeze(1)
-        a = params[:, 2].unsqueeze(1)
-        b = params[:, 3].unsqueeze(1)
+        dt_min = 0
+        dt_max = 0.005
+        f_min = 0.0
+        f_max = 0.7
+        dp_min = 0.005
+        dp_max = 0.5
+        c_min = 0.8
+        c_max = 1.2
 
-        c = a + b
-        fp = a / c  # (1 - fp) = b / c
+        dp = dp_min + params[:, 0].unsqueeze(1) * (dp_max - dp_min)
+        dt = dt_min + params[:, 1].unsqueeze(1) * (dt_max - dt_min)
+        fp = f_min + params[:, 2].unsqueeze(1) * (f_max - f_min)
+        c = c_min + params[:, 3].unsqueeze(1) * (c_max - c_min)
 
         x_fit = c * (fp * torch.exp(-self.b_values * dp)
-                     + (b / c)*torch.exp(-self.b_values * dt))
+                     + (torch.tensor(1) - fp)*torch.exp(-self.b_values * dt))
 
         return x_fit, dp, dt, fp, c
